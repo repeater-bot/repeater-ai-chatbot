f678464ed52df0c7bd6c39d376ac77eb654ecd70
Qeggs <get_boot@outlook.com>
2026-09-10 11:09:49 +0800

    feat(core): 增强文件获取功能并添加错误处理
    - 在PathFile类中添加异常处理，当无法读取本地文件时抛出HTTPException
    - 在UrlFile类中添加状态码检查，当URL请求失败时抛出HTTPException
    - 引入loguru日志记录器记录异常信息
    - 统一错误响应格式，提供更详细的错误描述信息

--- core/call_api/image/objects/auxiliary/files/url_file.py
+++ core/call_api/image/objects/auxiliary/files/url_file.py
@@ -2,6 +2,8 @@ import httpx
 
 from pydantic import BaseModel
 from typing import Literal
+from loguru import logger
+from ......special_exception import HTTPException
 
 class UrlFile(BaseModel):
     type: Literal["url"] = "url"
@@ -10,4 +12,12 @@ class UrlFile(BaseModel):
     async def get_file(self) -> bytes:
         async with httpx.AsyncClient() as client:
             response = await client.get(self.url)
+            logger.error(
+                "Server could not successfully retrieve the data provided."
+            )
+            if response.status_code != 200:
+                raise HTTPException(
+                    status_code = 400,
+                    detail = "Server could not successfully retrieve the data provided, please check the url and try again."
+                )
             return response.content
